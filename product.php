5cf08e68c58e9555e460d0e03e1fbb5644d1871e
Ong0429 <ong.zheng.hao@student.mmu.edu.my>
2026-03-24 23:28:35 +0800

    ok

--- product.php
+++ product.php
@@ -2,7 +2,7 @@
 session_start();
 include "db.php";
 
-// Redirect to login if not logged in,
+// Redirect to login if not logged in
 if (!isset($_SESSION['user'])) {
     header("Location: login.php");
     exit;
